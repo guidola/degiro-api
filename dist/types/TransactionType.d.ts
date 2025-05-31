3b76be88407763b58688354712d63a980788fb8e
Guillermo Dobao <guillermo.dobao@ailylabs.com>
2025-05-31 21:13:46 +0200

    chore: update transaction response

--- dist/types/TransactionType.d.ts
+++ dist/types/TransactionType.d.ts
@@ -8,10 +8,18 @@ export type TransactionType = {
     productId: number;
     quantity: number;
     total: number;
+    fxRate?: number;
+    nettFxRate?: number;
+    grossFxRate?: number;
+    autoFxFeeInBaseCurrency?: number;
     totalInBaseCurrency: number;
+    feeInBaseCurrency?: number;
+    totalFeesInBaseCurrency?: number;
     totalPlusFeeInBaseCurrency: number;
+    totalPlusAllFeesInBaseCurrency?: number;
     tradingVenue: string;
     transactionTypeId: number;
     transfered: boolean;
+    executingEntityId?: string;
 };
 //# sourceMappingURL=TransactionType.d.ts.map
